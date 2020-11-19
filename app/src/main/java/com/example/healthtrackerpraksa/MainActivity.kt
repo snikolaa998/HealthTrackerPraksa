ee5179f99ff3df8a558788ab27a1fcfbb28f7b2f
Nikola Savic <snikolaa998@gmail.com>
2020-11-19 12:35:48 +0100

    master - master activity done

--- app/src/main/java/com/example/healthtrackerpraksa/MainActivity.kt
+++ app/src/main/java/com/example/healthtrackerpraksa/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.healthtrackerpraksa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph_host) as NavHostFragment? ?: return
+        val navController = host.navController
+        setupBottomNavMenu(navController)
+    }
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_menu)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_graph_host))
+                || super.onOptionsItemSelected(item)
     }
 }
