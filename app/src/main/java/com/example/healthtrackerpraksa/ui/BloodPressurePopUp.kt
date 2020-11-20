ea4f327d776712be3913aa555849f3ea3ff40017
Nikola Savic <snikolaa998@gmail.com>
2020-11-20 10:01:47 +0100

    blood_pressure dodata klasa

---
+++ app/src/main/java/com/example/healthtrackerpraksa/ui/BloodPressurePopUp.kt
@@ -0,0 +1,17 @@
+package com.example.healthtrackerpraksa.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class BloodPressurePopUp : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
