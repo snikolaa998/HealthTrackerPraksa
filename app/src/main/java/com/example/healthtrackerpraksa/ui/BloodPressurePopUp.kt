8ec3acf8a0b7d584aa5cf904c7a6496cc90671d0
Nikola Savic <snikolaa998@gmail.com>
2020-11-20 15:34:31 +0100

    blood_pressure pop up done

--- app/src/main/java/com/example/healthtrackerpraksa/ui/BloodPressurePopUp.kt
+++ app/src/main/java/com/example/healthtrackerpraksa/ui/BloodPressurePopUp.kt
@@ -1,17 +1,35 @@
 package com.example.healthtrackerpraksa.ui
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import com.example.healthtrackerpraksa.R
 
 class BloodPressurePopUp : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        return super.onCreateDialog(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.dialog_add_blood_pressure, container, false)
+        val saveButton = view.findViewById<Button>(R.id.btn_save_blood_pressure)
+        val cancelButton = view.findViewById<Button>(R.id.btn_cancel_blood_pressure)
+        saveButton.setOnClickListener {
+            Toast.makeText(activity, "Akcija za save button", Toast.LENGTH_SHORT).show()
+        }
+        cancelButton.setOnClickListener {
+            Toast.makeText(activity, "Akcija za cancel button", Toast.LENGTH_SHORT).show()
+        }
+        return view
     }
 }
--- app/src/main/java/com/example/healthtrackerpraksa/ui/MainActivity.kt
+++ app/src/main/java/com/example/healthtrackerpraksa/ui/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.healthtrackerpraksa.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.example.healthtrackerpraksa.R
+import com.example.healthtrackerpraksa.ui.fragments.BloodPressure
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 //Totalno bezveze komentar
 //Totalno bezveze komentar2
@@ -17,10 +22,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Komentar
         val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph_host) as NavHostFragment? ?: return
         val navController = host.navController
+        val addButton = findViewById<FloatingActionButton>(R.id.floating_add_btn)
+        addButton.setOnClickListener {
+            getCurrentFragment()
+        }
         setupBottomNavMenu(navController)
+
     }
     private fun setupBottomNavMenu(navController: NavController) {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_menu)
@@ -30,5 +39,14 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return item.onNavDestinationSelected(findNavController(R.id.nav_graph_host))
                 || super.onOptionsItemSelected(item)
+    }
+    private fun getCurrentFragment() {
+        val currentFragment = findNavController(R.id.nav_graph_host).currentDestination
+        when(currentFragment?.label) {
+            "fragment_blood_pressure" -> {
+                val popUpDialog = BloodPressurePopUp()
+                popUpDialog.show(supportFragmentManager, "fragment_blood_pressure")
+            }
+        }
     }
 }
