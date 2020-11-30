548af703a3953ead6324dae832b937262fb77a6d
Nikola Pesic <nikolapesic992@hotmail.com>
2020-11-30 14:56:12 +0100

    slight reorganizing

---
+++ app/src/main/java/com/example/healthtrackerpraksa/util/uicomponents/temperaturegraph/SizeTransformations.kt
@@ -0,0 +1,28 @@
+package com.example.healthtrackerpraksa.util.uicomponents.temperaturegraph
+
+import android.content.Context
+import android.util.DisplayMetrics
+import android.util.TypedValue
+
+object SizeTransformations {
+
+    fun toDP(context: Context, value: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            value, context.resources.displayMetrics
+        )
+    }
+
+    fun toSP(context: Context, value: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            value,
+            context.resources.displayMetrics
+        )
+    }
+
+    fun pxToDp(context: Context, px: Int): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
+    }
+}
---
+++ app/src/main/java/com/example/healthtrackerpraksa/util/uicomponents/temperaturegraph/Temperature.kt
@@ -0,0 +1,7 @@
+package com.example.healthtrackerpraksa.util.uicomponents.temperaturegraph
+
+class Temperature(var time: Int, var temperature: Int) {
+
+
+
+}
