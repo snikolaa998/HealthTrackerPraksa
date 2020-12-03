e75d33edc2f2f71882c5d1a0cac97f11c7afedc4
Nikola Savic <snikolaa998@gmail.com>
2020-12-03 12:46:09 +0100

    SharedPref

--- app/src/main/java/com/example/healthtrackerpraksa/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/healthtrackerpraksa/ui/fragments/SettingsFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import com.example.healthtrackerpraksa.R
 
 
